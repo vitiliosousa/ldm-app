16cb65683d1ee4c2343c2af08d5def3a8ddd4da9
vitiliosousa <vitiliomartins2003@gmail.com>
2024-07-30 20:23:26 +0200

    logo da google

--- src/components/ButtonGoogle.tsx
+++ src/components/ButtonGoogle.tsx
@@ -1,10 +1,14 @@
+import Logo from '../../public/login/Google.svg'
+import Image from 'next/image'
+
 interface ButtonGoogleProps{
     texto: string
 }
 
 export default function ButtonGoogle({texto}: ButtonGoogleProps){
     return (
-        <button className="w-full text-black bg-white rounded font-bold h-12">
+        <button className="w-full text-black bg-white rounded font-bold h-12 flex items-center justify-center gap-2">
+            <Image src={Logo} alt='logo image' height={20} width={20}/>
             {texto}
         </button>
     )
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -8,7 +8,7 @@ export default function Input({label,placeholder}:InputProps){
     return(
         <div className="flex flex-col gap-1">
             <label className="font-bold">{label}</label>
-            <input type="text" placeholder={placeholder} className="w-full h-12 rounded bg-cinza placeholder-white px-5"/>
+            <input type="text" placeholder={placeholder} className="w-full h-12 rounded bg-cinza  px-5"/>
         </div>
     )
 }
